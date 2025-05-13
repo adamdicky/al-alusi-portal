4a64c3fcd64cc7216120ed5ce00a138dbb0d99b8
DrBoz1 <hesamstark99@gmail.com>
2025-05-13 14:04:13 +0800

    Added types for use states

--- src/components/CreateUser.tsx
+++ src/components/CreateUser.tsx
@@ -8,6 +8,11 @@ import Button from "@/components/Button";
 
 import { apiFetch } from "@/utils/functions/fetch";
 
+type Classroom = `${1 | 2 | 3 | 4 | 5 | 6}-${"UKM" | "UPM" | "USM"}`;
+
 export default function CreateUser({ close }: { close: () => void }) {
 	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [role, setRole] = useState<"Teacher" | "Admin" | "Staff Jabatan">("Teacher");
+	const [classroom, setClassroom] = useState<Classroom>();
 }
